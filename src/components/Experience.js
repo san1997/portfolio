f67369eb7725a50d813b4b57144d9df1ba385e10
Sandeep Pal <sandeep@itapgroup.com>
2023-07-28 00:07:00 +0530

    experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,7 +37,7 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={<img style={{width: "100%", "border-radius": "50%"}} src={work.companyLogo}/>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
