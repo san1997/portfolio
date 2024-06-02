6634fd4c3da4c05a51bd9d3779b9a55dda484848
Sandeep Pal <sandeep@itapgroup.com>
2024-06-02 17:55:46 +0530

    update package.json homepage and fix build path issue

---
+++ build/precache-manifest.0c50ca4507dc6e9b8f97cbbb083c6889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e3ae9efc76b7278ea32aa35bfc2529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a567064309fb13ad22",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e94df0d5b318883e6fdc",
+    "url": "/static/css/main.71794ba7.chunk.css"
+  },
+  {
+    "revision": "67a567064309fb13ad22",
+    "url": "/static/js/2.889b7614.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.889b7614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94df0d5b318883e6fdc",
+    "url": "/static/js/main.0bd2b7ad.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
